6d31131e52ea7e6562838d976f377c6f6eeac483
danivent23122020 <dani.vent@orange.fr>
2022-09-22 09:02:51 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// alert("toto");
+// *** scroll to top ***
+//display btn
+window.addEventListener("scroll", function () {
+    let scroll = this.document.querySelector(".btn-scrollup");
+    scroll.classList.toggle("active", window.scrollY > 200);
+});
+// scroll to top
+const btn = document.querySelector(".btn-scrollup");
+btn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
